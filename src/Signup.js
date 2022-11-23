5dbe4723600fd07b0a172d5a09b4c7c43caa5bc9
luolamies96 <tommijouppila@hotmail.com>
2022-11-23 10:22:38 +0200

    login ja signup

--- src/Signup.js
+++ src/Signup.js
@@ -4,58 +4,76 @@ import { useNavigate } from 'react-router-dom';
 import Constants from './Constants.json'
 
 export default function Signup() {
-
-    const [username, Setusername] = useState('')
-    const [password, SetPassword] = useState('')
-    const [email, SetEmail] = useState('')
-    
     var searchURL;
-    
+    const [ signupRrocessState, SetSignupProcessState] = useState("idle")
+
+
+
 
 
     const navigate = useNavigate();
 
     const handleSignupSubmit = async (event) =>{
         event.preventDefault();
-        Setusername(event.target.username.value);
-        SetPassword(event.target.password.value);
-        SetEmail(event.target.email.value);
-        
-        searchURL = Constants.API_ADDRESS+ "register?username=" + username + "&password=" + password + "&email=" + email
-      
+
+        SetSignupProcessState("processing");
+
+
+
+        searchURL = Constants.API_ADDRESS+ "register?username=" + event.target.username.value + "&password=" + event.target.password.value + "&email=" + event.target.email.value
+
        try{
-         await axios.post(searchURL);
-        navigate('/', {replace: true});
-       
+         const result =  await axios.post(searchURL);
+            console.log(result);
+            SetSignupProcessState("signupSuccess")
+             navigate('/login', {replace: true});
+
        }
        catch (error){
         console.error(error);
+        SetSignupProcessState("signupFailure")
        }
-       
+
+    }
+    let signupUIControls = null;
+    switch(signupRrocessState){
+        case "idle":
+            signupUIControls =   <button type="submit">Signup</button>
+            break;
+
+        case "processing":
+                signupUIControls = <span> Processing....</span>
+                break;
+        case "signupSuccess":
+            signupUIControls = <span style={{color:"green"}  } >signup success</span>
+            break;
+        case "signupFailure":
+        signupUIControls = <span style={{color:"red"}}>Error</span>
+        break;
     }
 
 
+    return (
+        <div>
+            <h2>Signup</h2>
+            <form onSubmit={handleSignupSubmit}>
+                <div>
+                    Username <br />
+                    <input type="text" name="username" />
+                </div>
+                <div>
+                    Password <br />
+                    <input type="text" name="password" />
+                </div>
+                <div>
+                    Email <br />
+                    <input type="text" name="email" />
+                </div>
+                <div>
+                    {signupUIControls}
+                </div>
 
-  return (
-    <div>
-        <h2>Signup</h2>
-        <form onSubmit={handleSignupSubmit}>
-            <div>
-                Username <br/>
-                <input type="text" name="username" />
-            </div>
-            <div>
-                Password <br/>
-                <input type="text" name="password" />
-            </div>
-            <div>
-                Email <br/>
-                <input type="text" name="email" />
-            </div>
-            <div>
-                <button type="submit">Signup</button>
-            </div>
-        </form>
-    </div>
-  )
+            </form>
+        </div>
+    )
 }
